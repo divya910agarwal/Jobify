45dc8e2e2207e0bd05c8ccf31118aa1fc9d3ab7c
divya910agarwal <DIVYA910DKB@GMAIL.COM>
2023-07-13 00:39:17 +0530

    alert added

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,29 +2,36 @@ import React from 'react'
 import { useState,useEffect } from 'react';
 import { Logo,FormRow,Alert } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
+import { useAppContext } from '../context/appContext';
 
 const initialState ={
     name:'',
     email:'',
     password:'',
     isMember:true,
-    showAlert: true,
+    
 }
 
 const Register = () => {
     const[values,setValues] = useState(initialState)
     //global state and useNAvigate
-
-const toggleMember =()=>{
-    setValues({...values,isMember:!values.isMember})
-}
+    const {isLoading,showAlert,displayAlert} = useAppContext();
+    
+    const toggleMember =()=>{
+        setValues({...values,isMember:!values.isMember})
+    }
 
     const handleChange = (e)=>{
-        console.log(e.target)
+        setValues({...values,[e.target.name]:e.target.value})
     }
     const onSubmit =(e)=>{
         e.preventDefault()
-        console.log(e.target)
+        const{name,email,password,isMember}= values
+        if(!email || !password ||(!isMember&&!name)){
+            displayAlert()
+            return
+        }
+
     }
     return (
         <Wrapper className='full-page'>
