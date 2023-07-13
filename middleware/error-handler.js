212f8c2bcd866f3738d71e9430b7b5c56f0e2904
divya910agarwal <DIVYA910DKB@GMAIL.COM>
2023-07-13 23:47:33 +0530

    Update error-handler.js

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,9 +1,13 @@
 /* eslint-disable prettier/prettier */
-
+import { StatusCodes } from "http-status-codes/build/cjs/status-codes";
 
 const errorHandlerMiddleware = (err, req, res, next) => {
     console.log(err);
-    res.status(500).json({ msg:err})
+    const defaultError ={
+        statusCode:StatusCodes.INTERNAL_SERVER_ERROR,
+        msg:'Something went wrong,try again later',
+    }
+    res.status(defaultError.statusCode).json({ msg:err})
 
 }
 
