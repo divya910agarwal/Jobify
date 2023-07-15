350a554b4dfa7d47e6fc5e9aa36a47a5c462aad9
divya910agarwal <DIVYA910DKB@GMAIL.COM>
2023-07-15 23:26:58 +0530

    errors handled

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,32 @@
 import User from "../models/user.js";
 import { StatusCodes } from "http-status-codes";
+import {BadRequestError} from '../errors/index.js'
+
+
+
+
+
+
 
 const register = async (req, res) => {
-    const user =await User.create(req.body)
+    const {name,email,password} = req.body
+
+    if(!name || !email || !password){
+      throw new BadRequestError('please provide all values')
+    }
+
+    const user =await User.create({ name, email, password})
     res.status(StatusCodes.CREATED).json({user})
   
-}
+
+  const userAlreadyExists = await User.findOne({email});
+  if(userAlreadyExists){
+    throw new BadRequestError('Email already exists')
+  }
+  const user = await User.create({ name, email,password})
+  res.status(StatusCodes.OK).json({ user })
+ 
+  }
 const login = async (req, res) => {
   res.send('login user');
 };
